851ec0bfbcca3af03b2b60b0f3cc903ee0f8d5ac
Pierre <pierre.puchaud@ens-rennes.fr>
2018-11-28 22:58:07 +0100

    update kinematic tutorial

--- Examples_Processed/1_SideStep_Kinematic_Processed/SideStep_Geometric_Calibration/PostProcessingKinematic_Calibration.m
+++ Examples_Processed/1_SideStep_Kinematic_Processed/SideStep_Geometric_Calibration/PostProcessingKinematic_Calibration.m
@@ -35,13 +35,15 @@ filename='ChgtDirection04';
 
 % Loading Inverse Kinematic results
 load(fullfile(filename,'InverseKinematicsResults.mat'))
+%Get the angles of interest
+q=InverseKinematicsResults.JointCoordinates(num_s,:)*180/pi ; % from radian to degrees
 
 %% Reconstruction Error
 % Computing the mean reconstruction error over the trial for each frames
 RE_mean = mean(InverseKinematicsResults.ReconstructionError,1);
 
-figure;
-plot(RE_mean,'b-','LineWidth',2);
+h1=figure('Name','Reconstruction Error');
+plot(RE_mean,'r-','LineWidth',2); hold on;
 xlabel('Frames');
 ylabel('Mean Reconstruction Error (m)');
 title(['Mean reconstruction error over the ' filename  ' trial for each frames'])
@@ -56,57 +58,83 @@ text(XX(2)/2,YY(1)+(YY(2)-YY(1))*2/3,'Mean Reconstruction Error',...
 text(XX(2)/2,YY(1)+(YY(2)-YY(1))*3/5,num2str(RE_global_mean),...
     'HorizontalAlignment','center')
 
+%loading previous results of anthropometric simulation
+load(fullfile('..\SideStep_Anthropo',filename,'InverseKinematicsResults.mat'))
+%Get the angles of interest
+q_anthropo=InverseKinematicsResults.JointCoordinates(num_s,:)*180/pi ; % from radian to degrees
+
+% Computing the mean reconstruction error over the trial for each frames
+RE_mean_anthropo = mean(InverseKinematicsResults.ReconstructionError,1);
+plot(RE_mean_anthropo,'b-','LineWidth',2); hold on;
+
+legend('Calibrated Model','Anthropometric Model')
+
+% Comments :
+% The reconstruction errors are globally reduced with the calibration
+% method
+
 %% Joint Angles
 
 % Number of frames
 Nb_frames = size(InverseKinematicsResults.JointCoordinates,2);
 
-% Get the angles of interest
-q=InverseKinematicsResults.JointCoordinates(num_s,:)*180/pi ; % from radian to degrees
-
-figure('Name','Right Leg Kinematic')
+h2=figure('Name','Right Leg Kinematic');
 
 subplot(2,3,1)
-plot(q(1,:),'b-','LineWidth',2);% in degrees
+plot(q(1,:),'r-','LineWidth',2);% in degrees
+hold on
+plot(q_anthropo(1,:),'b-','LineWidth',2);% in degrees
 xlabel('Frames')
 ylabel('Angle (°)')
 title([ Solids{1} ' First Z-axis Hip rotation'])
 xlim([0 Nb_frames])
 
 subplot(2,3,2)
-plot(q(2,:),'b-','LineWidth',2);% in degrees
+plot(q(2,:),'r-','LineWidth',2);% in degrees
+hold on
+plot(q_anthropo(2,:),'b-','LineWidth',2);% in degrees
 xlabel('Frames')
 ylabel('Angle (°)')
 title([ Solids{2} ' Second X-axis Hip rotation'])
 xlim([0 Nb_frames])
 
 subplot(2,3,3)
-plot(q(3,:),'b-','LineWidth',2);% in degrees
+plot(q(3,:),'r-','LineWidth',2);% in degrees
+hold on
+plot(q_anthropo(3,:),'b-','LineWidth',2);% in degrees
 xlabel('Frames')
 ylabel('Angle (°)')
 title([ Solids{3} ' Third Y-axis Hip rotation'])
 xlim([0 Nb_frames])
 
 subplot(2,3,4)
-plot(q(4,:),'b-','LineWidth',2);% in degrees
+plot(q(4,:),'r-','LineWidth',2);% in degrees
+hold on
+plot(q_anthropo(4,:),'b-','LineWidth',2);% in degrees
 xlabel('Frames')
 ylabel('Angle (°)')
 title([ Solids{4} ' Z-axis rotation - Knee Flexion/Extension'])
 xlim([0 Nb_frames])
 
 subplot(2,3,5)
-plot(q(5,:),'b-','LineWidth',2);% in degrees
+plot(q(5,:),'r-','LineWidth',2);% in degrees
+hold on
+plot(q_anthropo(5,:),'b-','LineWidth',2);% in degrees
 xlabel('Frames')
 ylabel('Angle (°)')
 title([ Solids{5} ' Second Z-axis rotation - Ankle Flexion/Extension'])
 xlim([0 Nb_frames])
 
 subplot(2,3,6)
-plot(q(5,:),'b-','LineWidth',2);% in degrees
+plot(q(6,:),'r-','LineWidth',2);% in degrees
+hold on
+plot(q_anthropo(6,:),'b-','LineWidth',2);% in degrees
 xlabel('Frames')
 ylabel('Angle (°)')
 title([ Solids{5} ' Second X-axis rotation - Ankle Inverserion/Eversion'])
 xlim([0 Nb_frames])
+
+legend('Calibrated Model','Anthropometric Model','location','best')
 
 % On the graph, we can see the angles from inverse kinematics for the 
 % selected angles of the lowerlimb. A 4th order zerolag
@@ -114,33 +142,3 @@ xlim([0 Nb_frames])
 % 5 Hz as it is specify in the Analysis parameters.
 
 
-%% What about the quality of the model ?
-% Shank distance
-% Experimental distance
-% loading experimental markers of the shank
-% real_markers = C3dProcessedData('ChgtDirection04',{'RKNI';'RKNE';'RANI';'RANE'});
-% % computing mean distance between knee markers and ankle markers
-% Xp_distance = norm( mean( (real_markers(3).position_c3d + real_markers(4).position_c3d)/2 ...
-%     - (real_markers(1).position_c3d + real_markers(2).position_c3d)/2,1 ));
-Xp_distance = 0.381; %m from the Range of Motion .c3d
-% Model distance
-% loading the model
-load('BiomechanicalModel.mat')
-% get the solid of reference
-Solid = 'RAnkle_J1';
-[~,num_s]=intersect(Solid_list,Solid,'stable');
-
-% Position of the Ankle Joint in the Shank reference frame
-Model_distance = norm(BiomechanicalModel.OsteoArticularModel(num_s).b);
-
-%Plot
-figure('Name','Shank length')
-bar([Xp_distance,Model_distance])
-xticklabels({'Mean Marker length','Model length'})
-title('Shank length');
-ylabel('Shank length (m)')
-
-% Conclusion
-% Distance from the anthropometric model is not corresponding to
-% experimental length...
-% We need a more robust calibration of the geometrical model.
